f66aa475c75b5d999314a4b0b5f3a9cffb316542
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-18 21:56:34 +0100

    Empty commit

--- walter-ci.sh
+++ walter-ci.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -xe
-echo "WALTER_GITHUB_USERNAME=${WALTER_GITHUB_USERNAME}" > lol.txt
-cat lol.txt
-echo "WALTER_GITHUB_USERNAME="
 lein deps
 lein test
 lein deploy github
-#lein deploy clojars
+lein deploy clojars
 exit 0
