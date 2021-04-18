2e3f3036ed858be64b8bb212787cb9485244e24e
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-18 18:24:26 +0100

    Bootstrap Walter CI

---
+++ test/piotr_yuxuan/walter_ci_test.clj
@@ -0,0 +1,6 @@
+(ns piotr-yuxuan.walter-ci-test
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(deftest walter-ci
+  (testing "walter-ci"
+    (is true)))
--- walter-ci.sh
+++ walter-ci.sh
@@ -1,2 +1,6 @@
 #!/bin/sh -xe
+lein deps
+lein test
+lein deploy github
+#lein deploy clojars
 exit 0
