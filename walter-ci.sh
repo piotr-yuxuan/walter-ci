6081c1aeb158a428c131b2857e92bbdb66a23598
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-20 18:01:50 +0100

    chmod +x walter-ci.sh

---
+++ walter-ci.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo foobar
+env $(env)
+echo foobar
+sudo apt-get install -y clojure
+clojure -Sdeps '{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version "LATEST"}}}}}' -M:walter-ci -m com.piotr-yuxuan.walter-ci.main
