09fcd736229d583bff53c88f91d1aff8bfd887a0
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-20 17:55:45 +0100

    Use action_path

---
+++ walter-ci.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install -y clojure
+clojure -Sdeps '{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version "LATEST"}}}}}' -M:walter-ci -m com.piotr-yuxuan.walter-ci.main
