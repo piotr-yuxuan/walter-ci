241aa38b3be6d12f0c64aa2ae26b4648389a647f
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-20 17:56:50 +0100

    Remove shabang

--- walter-ci.sh
+++ walter-ci.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 sudo apt-get install -y clojure
 clojure -Sdeps '{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version "LATEST"}}}}}' -M:walter-ci -m com.piotr-yuxuan.walter-ci.main
