3f7e808b1fc312ef1d65c47ba128e593d3ecbeda
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-20 18:04:42 +0100

    Arguments as strings

--- walter-ci.sh
+++ walter-ci.sh
@@ -3,4 +3,5 @@ echo foobar
 env $(env)
 echo foobar
 sudo apt-get install -y clojure
-clojure -Sdeps '{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version "LATEST"}}}}}' -M:walter-ci -m com.piotr-yuxuan.walter-ci.main
+clojure --help
+clojure '-Sdeps' '{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version "LATEST"}}}}}' '-M:walter-ci' '-m' 'com.piotr-yuxuan.walter-ci.main'
