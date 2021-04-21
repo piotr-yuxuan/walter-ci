11fce1bd75cfd54f62a8e8476c0657c1ca73cac1
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-21 20:10:59 +0100

    Try to run some simple commands

--- src/com/piotr_yuxuan/walter_ci/main.clj
+++ src/com/piotr_yuxuan/walter_ci/main.clj
@@ -3,4 +3,7 @@
 
 (defn -main
   [& args]
-  (println ::args args))
+  (println "I am bootstrapped :-)")
+  (shell/sh "lein" "deps" (System/getenv "GITHUB_WORKSPACE"))
+  (shell/sh "lein" "test" (System/getenv "GITHUB_WORKSPACE"))
+  (shell/sh "lein" "uberjar" (System/getenv "GITHUB_WORKSPACE")))
