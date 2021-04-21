bccf5aaa8029349a5b671a8ca7dbf6ed15676b60
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-21 20:30:36 +0100

    See if it works

--- src/com/piotr_yuxuan/walter_ci/main.clj
+++ src/com/piotr_yuxuan/walter_ci/main.clj
@@ -1,9 +1,19 @@
 (ns com.piotr-yuxuan.walter-ci.main
+  "All the environment variable in the [Action
+  reference](https://docs.github.com/en/actions/reference/environment-variables)
+  may be used. In addition, the following environment variables may be
+  used:
+
+  - WALTER_CLOJARS_USERNAME
+  - WALTER_CLOJARS_PASSWORD
+  - WALTER_GITHUB_USERNAME
+  - WALTER_GITHUB_PASSWORD"
+  (:require [clojure.java.shell :as shell])
   (:gen-class))
 
 (defn -main
   [& args]
-  (println "I am bootstrapped :-)")
+  (println "I am the first command run completely from a bootstrapped Walter :-)")
   (shell/sh "lein" "deps" (System/getenv "GITHUB_WORKSPACE"))
   (shell/sh "lein" "test" (System/getenv "GITHUB_WORKSPACE"))
   (shell/sh "lein" "uberjar" (System/getenv "GITHUB_WORKSPACE")))
