34eabd0024cd436c1bf421a4fc58202274c3a81a
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-23 22:08:38 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.github.piotr-yuxuan/walter-ci (-> "./resources/walter-ci.version" slurp .trim)
-  :description "A Clojure map which implements java.io.Closeable"
+  :description "Walter is the younger son of Helmut"
   :url "https://github.com/piotr-yuxuan/walter-ci"
   :license {:name "European Union Public License 1.2 or later"
             :url "https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12"
@@ -9,11 +9,20 @@
   :pom-addition [:developers [:developer
                               [:name "胡雨軒 Петр"]
                               [:url "https://github.com/piotr-yuxuan"]]]
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[leiningen "2.9.6"]
+                 [leiningen-core "2.9.6"]
+                 [medley "1.3.0"]
+                 [clj-http "3.12.1"]
+                 [com.arohner/uri "0.1.2"]
+                 [metosin/muuntaja "0.6.8"]
+                 [metosin/malli "0.4.0"]
+                 [metosin/jsonista "0.3.2"]]
   :global-vars {*warn-on-reflection* true}
-  :aot :all
+  :aot [piotr-yuxuan.walter-ci.main]
   :main com.piotr-yuxuan.walter-ci.main
-  :profiles {:dev {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=true"]}
+  :profiles {:github {:github/topics ["github" "actions" "automation" "clojure"]}
+             :provided {:dependencies [[org.clojure/clojure "1.10.3"]]}
+             :dev {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=true"]}
              :uberjar {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
                                   "-Dclojure.compiler.direct-linking=true"]}}
   :deploy-repositories [["clojars" {:sign-releases false
