43c7aee38a9b806bd2f03c9a957cca9df12fbf56
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-24 09:58:11 +0100

    Try to download and use versioned jar

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -6,5 +6,10 @@ id
 lein deps 1> /dev/null
 ls -hal
 export DEBUG=true
+WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
+export WALTER_CI_VERSION
+echo "WALTER_CI_VERSION=$WALTER_CI_VERSION"
+clojure -Sdeps "{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version \"${WALTER_CI_VERSION}\"}}}}}" -M:walter-ci -m piotr-yuxuan.walter-ci.main
+git status
 lein deploy clojars
 lein deploy github
