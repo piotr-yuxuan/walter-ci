0100de20e962191aa655077bc63bd3dc53d3a4ba
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-24 09:13:54 +0100

    Distinguish between action and repo

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -6,9 +6,7 @@ id
 lein deps
 ls -hal
 export DEBUG=true
-lein uberjar
-ls -hal target
-echo 'blah blah' > blah.txt
-cat blah.txt
-rm blah.txt
-docker build -t piotryuxuan/walter-ci:latest .
+
+cd "${GITHUB_ACTION_PATH}" || exit 1
+pwd
+lein run
