080cd179b9f98c9d24b944f225e88a280e29f2d5
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-24 22:53:26 +0100

    Remove unused files

--- index.js
+++
@@ -1,7 +0,0 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const {execSync, execFileSync} = require("child_process");
-
-execSync('pwd')
-execSync('ls -hal')
-
--- walter-ci.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo foobar
-env $(env)
-echo foobar
-sudo apt-get install -y clojure
-clojure --help
-clojure '-Sdeps' '{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version "LATEST"}}}}}' '-M:walter-ci' '-m' 'com.piotr-yuxuan.walter-ci.main'
