be4424d3e9be921166364a1ef1832e3e6aa5affd
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-24 10:29:14 +0100

    Clean main

--- src/piotr_yuxuan/walter_ci/main.clj
+++ src/piotr_yuxuan/walter_ci/main.clj
@@ -1,21 +1,9 @@
 (ns piotr-yuxuan.walter-ci.main
-  (:require [leiningen.core.project :as leiningen]
-            [medley.core :as medley]
+  (:require [medley.core :as medley]
             [camel-snake-kebab.core :as csk]
             [piotr-yuxuan.walter-ci.github :as github]
-            [jsonista.core :as json])
+            [piotr-yuxuan.walter-ci.install :as install])
   (:gen-class))
-
-(defn project-description
-  [{:keys [project-path] :as overrides}]
-  (merge {:version nil
-          :main-function nil
-          :docker-file nil
-          :license {:file nil
-                    :type nil}
-          :walter-ci {:installed? nil
-                      :standard? nil}}
-         overrides))
 
 (declare run-tests
          increment-version!
@@ -30,30 +18,13 @@
          new-github-release
          deploy-to-clojars)
 
-(defn clojure-library-leiningen
-  [project-path]
-  (-> (project-description {:project-path project-path})
-      (run-tests)
-      (increment-version!)
-      (upgrade-dependencies!)
-      (report-vulnerabilities)
-      (reverse-domain-based-project-group!)
-      (check-license!)
-      (list-licenses)
-      (quality-scan)
-      (lint-files!)
-      (sort-namespaces!)
-      (new-github-release)
-      (deploy-to-clojars)))
-
 (defn load-config
   []
-  {:env (->> (System/getenv)
-             (into {})
-             (medley/map-keys csk/->kebab-case-keyword))})
+  {:env (into {} (System/getenv))})
 
 (defn -main
   [& args]
   (let [config (load-config)]
     (github/step config)
+    (install/step config)
     (println :all-done)))
