0d6b6af1ae925d80ae1c28d37d55a681e867c91d
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-24 10:09:08 +0100

    Script walter-ci.sh now in a reasonable state

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -1,15 +1,3 @@
-set -x
-
-env
-pwd
-id
-lein deps 1> /dev/null
-ls -hal
-export DEBUG=true
+set -xe
 WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
-export WALTER_CI_VERSION
-echo "WALTER_CI_VERSION=$WALTER_CI_VERSION"
 clojure -Sdeps "{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version \"${WALTER_CI_VERSION}\"}}}}}" -M:walter-ci -m piotr-yuxuan.walter-ci.main
-git status
-lein deploy clojars
-lein deploy github
