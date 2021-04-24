221a35ff71b8c26bd2e817dc5d4c7dce169237f9
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-24 22:53:43 +0100

    Update walter-ci.sh

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -1,5 +1,7 @@
 set -xe
-cd "${GITHUB_ACTION_PATH}" || exit 1
-lein run
-#WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
-#clojure -Sdeps "{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version \"${WALTER_CI_VERSION}\"}}}}}" -M:walter-ci -m piotr-yuxuan.walter-ci.main
+WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
+mvn dependency:get -Dartifact=com.github.piotr-yuxuan:walter-ci:"${WALTER_CI_VERSION}":jar
+mvn dependency:copy -Dartifact=com.github.piotr-yuxuan:walter-ci:"${WALTER_CI_VERSION}":jar -DoutputDirectory="${GITHUB_ACTION_PATH}"
+# Doesn't work as of now, as we only send small jar and no uberjar to clojars
+#java -jar "walter-ci-${WALTER_CI_VERSION}".jar
+clojure -Sdeps "{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version \"${WALTER_CI_VERSION}\"}}}}}" -M:walter-ci -m piotr-yuxuan.walter-ci.main
