e63736f3bfc97640b2c11364167bccaffe1b6d58
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-24 13:05:54 +0100

    Self-hosted, gone wrong ^_^

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -1,3 +1,5 @@
 set -xe
-WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
-clojure -Sdeps "{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version \"${WALTER_CI_VERSION}\"}}}}}" -M:walter-ci -m piotr-yuxuan.walter-ci.main
+cd "${GITHUB_ACTION_PATH}" || exit 1
+lein run
+#WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
+#clojure -Sdeps "{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version \"${WALTER_CI_VERSION}\"}}}}}" -M:walter-ci -m piotr-yuxuan.walter-ci.main
