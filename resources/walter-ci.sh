475a09dda54d72381f0196e657d7d6da70610156
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-24 23:07:59 +0100

    Update walter-ci.sh

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -1,7 +1,4 @@
 set -xe
-cd "${GITHUB_ACTION_PATH}"
-lein uberjar
-cp ./target/*-standalone.jar "${GITHUB_ACTION_PATH}/walter-ci.jar"
-
-cd "${GTIHUB_WORKSPACE}"
-java -jar "${GITHUB_ACTION_PATH}/walter-ci.jar"
+cd "${GITHUB_ACTION_PATH}" || exit 1
+# Shockingly bad. See history for better attempts.
+lein run
