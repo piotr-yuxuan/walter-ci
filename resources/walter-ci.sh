7a6a996e13908f5545974e993dc97ac85ab15223
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-25 09:21:33 +0100

    Try to fix lein ancient

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -1,8 +1,10 @@
 set -xe
 WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
+
+cd "${GITHUB_ACTION_PATH}"
+lein ancient upgrade-profiles
 mkdir "${HOME}/.lein"
 cp "${GITHUB_ACTION_PATH}/resources/profiles.clj" "${HOME}/.lein/profiles.clj"
-lein ancient upgrade-profiles
 
 # Shockingly bad. See history for better attempts.
 cd "${GITHUB_ACTION_PATH}"
