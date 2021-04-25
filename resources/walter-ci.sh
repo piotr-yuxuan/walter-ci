9bcf2ab644810e1e64c47bc3777ddfad9d309f13
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-25 23:39:27 +0100

    System-wide Leiningen profiles
    It looks like the other strategy wasn't effective.

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -2,11 +2,12 @@ set -xe
 WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
 
 mkdir "${HOME}/.lein"
-cp "${GITHUB_ACTION_PATH}/profiles.clj" "${HOME}/.lein/profiles.clj"
+cp "${GITHUB_ACTION_PATH}/profiles.clj" "/etc/leiningen/profiles.clj"
 
 # Shockingly bad. See history for better attempts.
 cd "${GITHUB_ACTION_PATH}"
 lein install
 
 cd "${GITHUB_WORKSPACE}"
+lein help
 clojure -Sdeps "{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version \"${WALTER_CI_VERSION}\"}}}}}" -M:walter-ci -m piotr-yuxuan.walter-ci.main
