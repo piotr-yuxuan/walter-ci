e91ac531a5234c08670d4de492bbe95b593c0e10
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-25 08:18:07 +0100

    Where is GitHub context?

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -1,5 +1,9 @@
 set -xe
 env
+
+ls -hal /home/runner/work/_temp/_github_workflow/event.json
+cat /home/runner/work/_temp/_github_workflow/event.json
+
 cd "${GITHUB_ACTION_PATH}"
 # Shockingly bad. See history for better attempts.
 WALTER_CI_VERSION=$(awk '{$1=$1};1' < "./resources/walter-ci.version")
