0d13aa779b2c2f0a0ba2a65244363f8368397b2e
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-25 23:31:53 +0100

    Delete useless ns

--- src/piotr_yuxuan/walter_ci/lein_deploy.clj
+++
@@ -1,7 +0,0 @@
-(ns piotr-yuxuan.walter-ci.lein-deploy
-  (:require [babashka.process :as process]
-            [clojure.java.io :as io]
-            [leiningen.core.project :as leiningen]
-            [leiningen.deploy]))
-
-
