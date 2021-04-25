ce17984b960bfdff4d86af948f6d2b47c3118874
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-25 23:59:15 +0100

    Leiningen profiles
    See the definition of LEIN_HOME in GitHUb action env vars, as well as
    here:
    https://github.com/technomancy/leiningen/blob/4d8ee78018158c05d69b250e7851f9d7c3a44fac/leiningen-core/src/leiningen/core/user.clj#L88

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -1,14 +1,7 @@
 set -xe
 WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
 
-mkdir "${HOME}/.lein"
-cp "${GITHUB_ACTION_PATH}/profiles.clj" "${HOME}/.lein/profiles.clj"
-
-mkdir "${XDG_CONFIG_HOME}/.lein"
-cp "${GITHUB_ACTION_PATH}/profiles.clj" "${XDG_CONFIG_HOME}/.lein/profiles.clj"
-
-mkdir "${RUNNER_WORKSPACE}/.lein"
-cp "${GITHUB_ACTION_PATH}/profiles.clj" "${RUNNER_WORKSPACE}/.lein/profiles.clj"
+cp "${GITHUB_ACTION_PATH}/profiles.clj" "${LEIN_HOME}/profiles.clj"
 
 # Shockingly bad. See history for better attempts.
 cd "${GITHUB_ACTION_PATH}"
