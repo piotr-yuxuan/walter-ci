f7a659b62be8d9c26fde49d351a81b0bd8013f29
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-08-08 14:49:02 +0100

    Don't copy profiles
    (what changed??)

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -6,7 +6,6 @@ set -xe
 cd "${GITHUB_ACTION_PATH}"
 lein install
 
-cp "${GITHUB_ACTION_PATH}/profiles.clj" "${LEIN_HOME}/profiles.clj"
 cd "${GITHUB_WORKSPACE}"
 WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
 clojure -Sdeps "{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version \"${WALTER_CI_VERSION}\"}}}}}" -M:walter-ci -m piotr-yuxuan.walter-ci.main
