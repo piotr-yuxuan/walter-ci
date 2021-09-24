795d9587dc2271dec235b0a0c9babbf3113d9a88
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-09-24 20:39:53 +0100

    Try correct secret upsert

--- src/piotr_yuxuan/walter_ci/main.clj
+++ src/piotr_yuxuan/walter_ci/main.clj
@@ -1,6 +1,7 @@
 (ns piotr-yuxuan.walter-ci.main
   "Hint: caesium appears to run on Java SDK 14, but not 17."
   (:require [camel-snake-kebab.core :as csk]
+            [clj-yaml.core :as yaml]
             [leiningen.change]
             [caesium.crypto.box]
             [clojure.data]
@@ -9,9 +10,8 @@
             [jsonista.core :as json]
             [clojure.java.io :as io]
             [clojure.edn :as edn])
-  (:gen-class)
-  (:import (java.util Base64)
-           (java.nio.charset StandardCharsets)))
+  (:import (java.util Base64))
+  (:gen-class))
 
 (defn public-key
   [{:keys [github-api-url github-actor walter-github-password]} github-repository]
@@ -25,7 +25,7 @@
                      json/read-value)
         ^String encoded-key (get payload "key")]
     {:encoded-key encoded-key
-     :decoded-key (.decode (Base64/getDecoder) (.getBytes encoded-key StandardCharsets/UTF_8))
+     :decoded-key (.decode (Base64/getDecoder) (.getBytes encoded-key))
      :key-id (get payload "key_id")}))
 
 (defn sealed-public-key-box
@@ -37,32 +37,28 @@
 
 (defn upsert-secret-value
   [{:keys [github-api-url github-actor walter-github-password]} target-repository secret-name sealed-public-key-box]
-  (try
-    (http/request
-      {:request-method :put
-       :url (str/join "/" [github-api-url "repos" target-repository "actions" "secrets" (csk/->SCREAMING_SNAKE_CASE_STRING secret-name)])
-       :body (json/write-value-as-string sealed-public-key-box)
-       :basic-auth [github-actor walter-github-password]
-       :headers {"Content-Type" "application/json"
-                 "Accept" "application/vnd.github.v3+json"}})
-    (catch Exception ex
-      (println (pr-str (ex-data ex)))
-      (throw ex))))
+  (http/request
+    {:request-method :put
+     :url (str/join "/" [github-api-url "repos" target-repository "actions" "secrets" (csk/->SCREAMING_SNAKE_CASE_STRING secret-name)])
+     :body (json/write-value-as-string sealed-public-key-box)
+     :basic-auth [github-actor walter-github-password]
+     :headers {"Content-Type" "application/json"
+               "Accept" "application/vnd.github.v3+json"}}))
+
+(defn spit-workflow-yaml
+  "Did you know that the alpha2 country code of Norway is `false`, and
+  `on` is `true`? I just refuse to touch a file format so corrupted by
+  syntactic sugar. Read edn data in `input-file` and write yaml
+  equivalent in `output-file`. Prepend a header."
+  [form output-filename]
+  (as-> form $
+    (yaml/generate-string $ :dumper-options {:flow-style :block})
+    (str (slurp (io/resource "header.yaml")) $)
+    (spit output-filename $ :append false)))
 
 (defn -main
   [& _]
-  (let [github-repository (System/getenv "GITHUB_REPOSITORY")
-        config {:github-repository github-repository
-                :github-api-url (System/getenv "GITHUB_API_URL")
-                :github-actor (System/getenv "GITHUB_ACTOR")
-                :walter-github-password (System/getenv "WALTER_GITHUB_PASSWORD")}]
-    (doseq [github-repository (:github-repositories (edn/read-string (slurp (io/resource "state.edn"))))
-            {:keys [secret-name secret-value]} [{:secret-name "MY_SECRET" :secret-value "Value set by an action."}]]
-      (println :github-repository github-repository
-               :secret-name secret-name)
-      (let [public-key (public-key config github-repository)]
-        (->> (sealed-public-key-box public-key secret-value)
-             (upsert-secret-value config github-repository secret-name))))))
+  (println :ok))
 
 ;;; Trigger documentation build:
 ;;; curl --verbose 'https://cljdoc.org/api/request-build2' \
