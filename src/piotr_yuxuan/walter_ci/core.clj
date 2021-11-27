bafc7759e5a02e6ff827a26d4235bdbe19fe72b0
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-11-27 07:40:52 +0000

    Clean-up, and faster exit

--- src/piotr_yuxuan/walter_ci/core.clj
+++ src/piotr_yuxuan/walter_ci/core.clj
@@ -19,13 +19,5 @@
   [{:keys [input-command github-action-path managed-repositories] :as config}]
   (cond (= :copy-workflows input-command)
         (doseq [github-repository managed-repositories]
-
-          (println ::start github-repository)
-          (println ::workflow-file (.exists (->file github-action-path)) (str (->file github-action-path)))
-          (println ::workflow-file (.exists (->file github-action-path "resources")) (str (->file github-action-path "resources")))
-          (println ::workflow-file (.exists (->file github-action-path "resources" "workflows")) (str (->file github-action-path "resources" "workflows")))
-          (println ::workflow-file (.exists (->file github-action-path "resources" "workflows" "dummy-workflow.yml")) (str (->file github-action-path "resources" "workflows" "dummy-workflow.yml")))
-
-          (println ::github-repository github-repository)
           (copy-workflow (assoc config :github-repository github-repository)
                          (->file github-action-path "resources" "workflows" "dummy-workflow.yml")))))
--- src/piotr_yuxuan/walter_ci/main.clj
+++ src/piotr_yuxuan/walter_ci/main.clj
@@ -11,9 +11,8 @@
 (defn -main
   [& args]
   (let [config (load-config args)]
-    (println (System/getenv "INPUT_COMMAND"))
-    (cond (:show-config? config) (do (clojure.pprint/pprint config) (System/exit 0))
-          (:help config) (do (println (malli-cli/summary Config)) (System/exit 0))
+    (cond (:show-config? config) (do (clojure.pprint/pprint config))
+          (:help config) (do (println (malli-cli/summary Config)))
 
           (not (m/validate Config config))
           (do (println "Invalid configuration value")
@@ -24,4 +23,5 @@
                                           me/humanize))
               (System/exit 1))
 
-          :else (core/start config))))
+          :else (core/start config)))
+  (System/exit 0))
