9a2abaedac61e10ab6e9988e104239ea293f197f
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-12-17 21:22:53 +0000

    Download uberjar from GitHub latest release

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -2,11 +2,8 @@ set -xe
 # Unfathomably bad. See history for better attempts. Instead of
 # downloading and installing dependencies every time, we should
 # download a small, compact binary and run it directly.
-cd "${GITHUB_ACTION_PATH}"
-lein install
-
 cp "${GITHUB_ACTION_PATH}/resources/profiles.clj" "${LEIN_HOME}/profiles.clj"
-
+export WALTER_CI_JAR="${GITHUB_ACTION_PATH}/walter-ci-standalone.jar"
+wget https://github.com/piotr-yuxuan/walter-ci/releases/latest/download/walter-ci-standalone.jar -O "${WALTER_CI_JAR}"
 cd "${GITHUB_WORKSPACE}"
-WALTER_CI_VERSION=$(awk '{$1=$1};1' < "${GITHUB_ACTION_PATH}/resources/walter-ci.version")
-clojure -Sdeps "{:aliases {:walter-ci {:replace-deps {com.github.piotr-yuxuan/walter-ci {:mvn/version \"${WALTER_CI_VERSION}\"}}}}}" -M:walter-ci -m piotr-yuxuan.walter-ci.main
+java -jar "${WALTER_CI_JAR}"
