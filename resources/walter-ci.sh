9c257460d5d426e701dcc4391804b2f33f86c513
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-12-19 10:33:04 +0000

    Hide event contents

--- resources/walter-ci.sh
+++ resources/walter-ci.sh
@@ -7,5 +7,4 @@ export WALTER_CI_JAR="${GITHUB_ACTION_PATH}/walter-ci-standalone.jar"
 # FIXME We should take advantage of GitHub action caching.
 wget --quiet https://github.com/piotr-yuxuan/walter-ci/releases/latest/download/walter-ci-standalone.jar -O "${WALTER_CI_JAR}"
 cd "${GITHUB_WORKSPACE}"
-cat "${GITHUB_EVENT_PATH}"
 java -jar "${WALTER_CI_JAR}"
