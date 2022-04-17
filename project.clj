5b63a03a6f6f0ae5c903bd3ff74ad9f8275e7576
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2022-04-17 21:43:11 +0100

    Clean-up of project.clj

--- project.clj
+++ project.clj
@@ -9,8 +9,7 @@
   :pom-addition [:developers [:developer
                               [:name "胡雨軒 Петр"]
                               [:url "https://github.com/piotr-yuxuan"]]]
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [leiningen "2.9.6" :upgrade false :exclusions [org.apache.httpcomponents/httpcore]]
+  :dependencies [[leiningen "2.9.6" :upgrade false :exclusions [org.apache.httpcomponents/httpcore]]
                  [leiningen-core "2.9.6" :upgrade false]
                  [com.brunobonacci/safely "0.7.0-alpha3"]
                  [clj-http "3.12.3" :exclusions [riddley]]
@@ -18,15 +17,14 @@
                  [babashka/process "0.1.1"]
                  [camel-snake-kebab "0.4.2"]
                  [metosin/malli "0.8.4"]
-                 [io.forward/yaml "1.0.11"] ; No same person would wilfully accept to write YAML by hand.
+                 [io.forward/yaml "1.0.11"] ; No sane person would wilfully accept to write YAML by hand.
                  [caesium "0.14.0"]
                  [metosin/jsonista "0.3.5"]]
   :main piotr-yuxuan.walter-ci.main
   :profiles {:github {:github/topics ["github" "actions" "automation" "clojure"]
                       :github/private? false}
              :provided {:dependencies [[org.clojure/clojure "1.11.1"]]}
-             :dev {:global-vars {*warn-on-reflection* true}
-                   :plugins [[lein-binplus "0.6.6"]]}
+             :dev {:global-vars {*warn-on-reflection* true}}
              :uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"
                                   "-Dclojure.compiler.disable-locals-clearing=false"
