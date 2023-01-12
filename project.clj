b4da054370cc2fbe569374be206e9e24c401c2b2
Walter CI <piotr-yuxuan@users.noreply.github.com>
2023-01-12 18:43:27 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                  [babashka/process "0.4.14"]
                  [camel-snake-kebab "0.4.3"]
                  [de.ubercode.clostache/clostache "1.4.0"] ; Templating engine Clojure wrapper.
-                 [metosin/malli "0.9.2"]
+                 [metosin/malli "0.10.0"]
                  [io.forward/yaml "1.0.11"] ; No sane person would wilfully accept to write YAML by hand.
                  [caesium "0.15.0"]
                  [metosin/jsonista "0.3.7"]]
