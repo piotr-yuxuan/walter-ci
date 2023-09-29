c642c21581421c4ea5e623f1279e13a01aa2c934
Walter CI <piotr-yuxuan@users.noreply.github.com>
2023-09-29 06:36:29 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                  [metosin/malli "0.13.0"]
                  [io.forward/yaml "1.0.11"] ; No sane person would wilfully accept to write YAML by hand.
                  [caesium "0.15.0"]
-                 [metosin/jsonista "0.3.7"]]
+                 [metosin/jsonista "0.3.8"]]
   :main piotr-yuxuan.walter-ci.main
   :profiles {:github {:github/topics ["github" "actions" "automation" "clojure"]
                       :github/private? false}
