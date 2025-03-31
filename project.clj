aef41c2ea6d4f21412540651cac793458da80c7a
Walter CI <piotr-yuxuan@users.noreply.github.com>
2025-03-31 18:47:07 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [com.brunobonacci/safely "1.0.0"]
                  [clj-http "3.13.0" :exclusions [riddley]]
                  [com.github.piotr-yuxuan/malli-cli "2.1.2"] ; Configuration value from the command-line
-                 [babashka/process "0.5.22"]
+                 [babashka/process "0.6.23"]
                  [camel-snake-kebab "0.4.3"]
                  [de.ubercode.clostache/clostache "1.4.0"] ; Templating engine Clojure wrapper.
                  [metosin/malli "0.17.0"]
